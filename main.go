4df05e5a58ee98ea05bb770ca61d575a820899b4
ghm20020 <m20020@g.metro-cit.ac.jp>
2024-01-09 14:34:24 +0900

    変更1

--- main.go
+++ main.go
@@ -22,6 +22,6 @@ func Hello() echo.HandlerFunc {
 
 func ApiHelloGet() echo.HandlerFunc {
     return func(c echo.Context) error {
-        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4407", "message": "hello"})
+        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4407", "message": "nice"})
     }
 }
